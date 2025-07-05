1e641c41a017b5d5f68347f157863d389aec20e7
MLior8dCode <elmeddin.mohubbetov2@gmail.com>
2025-07-05 14:00:32 +0400

    CustomerRepository.kt

--- coffee-shop-application/src/main/kotlin/com/motycka/edu/customer/CustomerRepository.kt
+++ coffee-shop-application/src/main/kotlin/com/motycka/edu/customer/CustomerRepository.kt
@@ -1,7 +1,30 @@
-package com.motycka.edu.customer
+package com.motycka.edu
 
-import com.motycka.edu.user.UserId
+import com.motycka.edu.customer.CustomerTable
+import com.motycka.edu.menu.MenuItemTable
+import com.motycka.edu.order.OrderTable
+import com.motycka.edu.user.UserTable
+import kotlinx.coroutines.Dispatchers
+import org.jetbrains.exposed.sql.*
+import org.jetbrains.exposed.sql.transactions.experimental.newSuspendedTransaction
+import org.jetbrains.exposed.sql.transactions.transaction
 
-interface CustomerRepository {
-    fun selectCustomer(userId: UserId): CustomerDTO?
+// I certainly hope it is going this way
+
+fun configureTestDatabase(vararg tables: Table) {
+    Database.connect(
+        url = "jdbc:h2:mem:test;DB_CLOSE_DELAY=-1;",
+        driver = "org.h2.Driver"
+    )
+
+    transaction {
+        SchemaUtils.create(*tables)
+    }
+}
+
+
+suspend fun <T> testTransaction(block: suspend () -> T): T {
+    return newSuspendedTransaction(Dispatchers.IO) {
+        block()
+    }
 }
